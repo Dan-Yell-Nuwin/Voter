9524615a5bfb85f58de09c9d2d7ae939d24aa17c
Kunwar Shani <kunwar.sahni01@gmail.com>
2019-10-12 14:46:39 -0400

    work on local page begun

---
+++ voter/voter/StateViewController.swift
@@ -0,0 +1,43 @@
+//
+//  StateViewController.swift
+//  voter
+//
+//  Created by Kunwar Sahni on 10/12/19.
+//  Copyright © 2019 mhacks. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class StateViewController: UIViewController {
+
+    @IBOutlet weak var currIndGovImg: UIImageView!
+    @IBOutlet weak var currIndGovNm: UILabel!
+    @IBOutlet weak var indGovCandImg: UIImageView!
+    @IBOutlet weak var indGovCandNm: UILabel!
+    @IBOutlet weak var currIndRepImg: UIImageView!
+    @IBOutlet weak var currIndRepNm: UILabel!
+    @IBOutlet weak var indRedCandImg: UIImageView!
+    @IBOutlet weak var indRedCandNm: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func currIndGov(_ sender: Any) {
+        
+    }
+    
+    @IBAction func indGovCand(_ sender: Any) {
+        
+    }
+    
+    @IBAction func curIndRep(_ sender: Any) {
+        
+    }
+    
+    @IBAction func indRepCand(_ sender: Any) {
+        
+    }
+}
